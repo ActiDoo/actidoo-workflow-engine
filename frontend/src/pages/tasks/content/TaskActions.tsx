91a01f0e8285d73d260002f15571cd1026d8a8ca
Janzen, Jean-Luca (QQOWTD) <jean-luca.janzen@phoenixcontact.com>
2026-06-30 14:22:09 +0000

    adjusted the taskactions.tsx

--- frontend/src/pages/tasks/content/TaskActions.tsx
+++ frontend/src/pages/tasks/content/TaskActions.tsx
@@ -13,15 +13,7 @@ export const TaskActions: React.FC<TaskActionsProps> = props => {
   const { t } = useTranslation();
   return (
     <>
-      <div className="flex flex-row flex-wrap gap-2 mt-16 ">
-        <button
-          disabled={!!props.disabled}
-          type="button"
-          className="btn btn-secondary min-w-[200px] max-h-[38px]"
-          onClick={props.onReset}>
-          {t('taskActions.reset')}
-        </button>
-        <div className="flex-1"></div>
+      <div className="flex flex-row flex-wrap gap-2">
         <div className="flex flex-column gap-2">
           <button
             disabled={!!props.disabled}
@@ -30,6 +22,21 @@ export const TaskActions: React.FC<TaskActionsProps> = props => {
             {t('taskActions.submit')}
           </button>
         </div>
+        <div className="flex-1"></div>
+        <button
+          disabled={!!props.disabled}
+          type="button"
+          className="btn btn-secondary max-h-[38px]"
+          onClick={props.onReset}>
+          {t('taskActions.reset')}
+        </button>
+        <button
+          disabled={!!props.disabled}
+          type="button"
+          className="btn btn-secondary max-h-[38px]"
+          onClick={props.onReset}>
+          {t('DeletePlaceholder')}
+        </button>
       </div>
     </>
   );
